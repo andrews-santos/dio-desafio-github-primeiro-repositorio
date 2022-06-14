64b64d8c74ef93faae2edbf9e2b0f5610aa227d1
andrews-santos <andrews.sant@gmail.com>
2022-06-13 21:16:18 -0300

    Finalizacao do Arquivo

--- 004 - Introdução ao JavaScript/Contador/assets/js/scripts.js
+++ 004 - Introdução ao JavaScript/Contador/assets/js/scripts.js
@@ -1,15 +1,28 @@
-var currentNumberWrapper /*tag span do html*/ = document.getElementById("currentNumber");
+var currentNumberWrapper = document.getElementById("currentNumber");
 var currentNumber = 0;
-
+              
 function increment() {
-    currentNumber = currentNumber + 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+    if (currentNumber <= 9  ) {
+            currentNumber = currentNumber + 1;
+            currentNumberWrapper.style.color = "white";   
+    if (currentNumber < 0 ){
+            currentNumberWrapper.style.color = "red";
+        } 
+    } 
+currentNumberWrapper.innerHTML = currentNumber;
+console.log(currentNumberWrapper,currentNumber);
 }
 
 function decrement() {
-    currentNumber = currentNumber - 1;
+    if (currentNumber >= -9 ) {
+        if (currentNumber <= 0 ){
+            currentNumberWrapper.style.color = "red";
+        }
+        currentNumber = currentNumber -1;
+    }  
     currentNumberWrapper.innerHTML = currentNumber;
+    console.log(currentNumberWrapper,currentNumber);                     
 }
 
 
-//https://github.com/stebsnusch/basecamp-javascript/tree/main/introducao-ao-javascript/contador
+
