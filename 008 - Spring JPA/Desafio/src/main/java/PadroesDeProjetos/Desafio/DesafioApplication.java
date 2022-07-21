c65a1b8c0cd5e08f19ff044186c71b8582620177
andrews-santos <andrews.sant@gmail.com>
2022-07-20 22:35:08 -0300

    Consumindo API Busca Cep Com Spring

---
+++ 008 - Spring JPA/Desafio/src/main/java/PadroesDeProjetos/Desafio/DesafioApplication.java
@@ -0,0 +1,16 @@
+package PadroesDeProjetos.Desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class DesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApplication.class, args);
+		
+	}
+
+}
---
+++ 008 - Spring JPA/Desafio/src/main/java/PadroesDeProjetos/Desafio/service/ClienteService.java
@@ -0,0 +1,19 @@
+package PadroesDeProjetos.Desafio.service;
+
+import PadroesDeProjetos.Desafio.model.Cliente;
+
+
+
+public interface ClienteService {
+
+	Iterable<Cliente> buscarTodos();
+
+	Cliente buscarPorId(Long id);
+
+	void inserir(Cliente cliente);
+
+	void atualizar(Long id, Cliente cliente);
+
+	void deletar(Long id);
+
+}
